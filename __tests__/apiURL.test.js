d60569f1fe31114f29fbba56cb35bcc1e29f291f
Crio.GitLab <admin@example.com>
2020-09-30 15:01:41 +0550

    updated changes - 2020-09-30 14:41:41

--- __tests__/apiURL.test.js
+++
@@ -1,7 +0,0 @@
-const functions = require('../leetcode');
-
-describe('API URL:', () => {
-  it('It must be correct', () => {
-    expect(functions.getApiURL()).toEqual('https://leetcode.com/api/problems/all/');
-  });
-});
